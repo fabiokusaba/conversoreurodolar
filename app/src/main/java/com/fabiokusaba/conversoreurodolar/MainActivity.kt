09999de6ccc2212bac699ce5c1c717be3a6c94b9
Fabio Kusaba <fabinhokb44@gmail.com>
2023-09-26 20:13:57 -0300

    add: leitura dados, converão euro/dollar, estilização

--- app/src/main/java/com/fabiokusaba/conversoreurodolar/MainActivity.kt
+++ app/src/main/java/com/fabiokusaba/conversoreurodolar/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.fabiokusaba.conversoreurodolar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fabiokusaba.conversoreurodolar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener {
+            val euros = binding.editEuro.text.toString().toDouble()
+            val dolares = String.format("%.2f", euros * 0.8)
+
+            binding.textDolares.text = "$dolares $"
+        }
     }
 }
