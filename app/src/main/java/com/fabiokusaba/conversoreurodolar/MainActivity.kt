748e52744318d95d72175b52fa3e153a507c2963
Fabio Kusaba <fabinhokb44@gmail.com>
2023-09-26 23:05:57 -0300

    add: novos tipos de conversão, implementação do uso do Toast

--- app/src/main/java/com/fabiokusaba/conversoreurodolar/MainActivity.kt
+++ app/src/main/java/com/fabiokusaba/conversoreurodolar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.fabiokusaba.conversoreurodolar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fabiokusaba.conversoreurodolar.databinding.ActivityMainBinding
 
@@ -11,11 +12,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonConverter.setOnClickListener {
-            val euros = binding.editEuro.text.toString().toDouble()
-            val dolares = String.format("%.2f", euros * 0.8)
+        binding.buttonDolar.setOnClickListener {
+            converterValor(0.8)
+        }
 
-            binding.textDolares.text = "$dolares $"
+        binding.buttonPeso.setOnClickListener {
+            converterValor(10.2)
+        }
+
+        binding.buttonReal.setOnClickListener {
+            converterValor(5.0)
+        }
+    }
+
+    private fun converterValor(taxa: Double) {
+        val euros = binding.editEuros.text.toString().trim()
+
+        if (!euros.isEmpty()) {
+            val dolares = String.format("%.2f", euros.toDouble() * taxa)
+            Toast.makeText(applicationContext, "$dolares$", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext, "Por favor, insira um valor em Euro", Toast.LENGTH_SHORT).show()
         }
     }
 }
